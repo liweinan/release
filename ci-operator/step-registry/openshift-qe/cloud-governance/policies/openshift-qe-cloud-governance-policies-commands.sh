d46e5c27ee34d33a5b563664fb26c55b2a290b38
阿男 <l.weinan@gmail.com>
2025-07-21 22:39:00 +0800

    OpenShift Installer CORS-4133 CI implementation: Add support for Taiwan ap-southeast-6 in AWS

--- ci-operator/step-registry/openshift-qe/cloud-governance/policies/openshift-qe-cloud-governance-policies-commands.sh
+++ ci-operator/step-registry/openshift-qe/cloud-governance/policies/openshift-qe-cloud-governance-policies-commands.sh
@@ -30,7 +30,7 @@ function run_policy() {
         export policy
         regions=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "eu-north-1" "eu-west-3" "eu-west-2"
                  "eu-west-1" "ap-northeast-3" "ap-northeast-2" "ap-northeast-1" "ca-central-1" "sa-east-1"
-                 "ap-southeast-1" "ap-southeast-2" "eu-central-1")
+                 "ap-southeast-1" "ap-southeast-2" "ap-southeast-6" "eu-central-1")
         for region in "${regions[@]}"; do
             export AWS_DEFAULT_REGION="${region}"
             if [[ -n "$s3_bucket" ]]; then
